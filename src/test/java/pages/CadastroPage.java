4e183397ef7c44699da07549add598fe95aa5312
Pedro Junio <pedrofjduarte@gmail.com>
2021-10-10 20:19:29 -0300

    Preenchimento do cadastro. Campos: Title, First name, Last name, Email, Password  e Date of Birth.

--- src/test/java/pages/CadastroPage.java
+++ src/test/java/pages/CadastroPage.java
@@ -1,4 +1,60 @@
 package pages;
 
-public class CadastroPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import support.Utils;
+
+public class CadastroPage extends Utils {
+
+    WebDriver driver;
+    private By titleM = By.id("id_gender1");
+    private By titleF = By.id("id_gender2");
+
+    private By first_name_field = By.id("customer_firstname");
+    private By last_name_field = By.id("customer_lastname");
+    //private By email_field = By.id("id_gender2"); esse campo já vem preenchido.
+    private By password_field = By.id("passwd");
+
+    private By select_day_field = By.id("days");
+    private By select_months_field = By.id("months");
+    private By select_years_field = By.id("years");
+
+    public CadastroPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void selectTitle(Integer type){
+
+        esperarElementoEstarPresente(titleM, 20);
+
+        if(type == 1){
+            driver.findElement(titleM).click();
+        }else if(type == 2){
+            driver.findElement(titleF).click();
+        }
+    }
+
+    public void preencheNome(String name){
+        driver.findElement(first_name_field).sendKeys(name);
+    }
+
+    public void preencheSobrenome(String sobreNome){
+        driver.findElement(last_name_field).sendKeys(sobreNome);
+    }
+
+    public void preenchePassword(String password){
+        driver.findElement(password_field).sendKeys(password);
+    }
+
+    public void selecionaAniversario(Integer day, Integer month, String year){
+        Select select_day = new Select(driver.findElement(select_day_field));
+        select_day.selectByIndex(day);
+
+        Select select_month = new Select(driver.findElement(select_months_field));
+        select_month.selectByIndex(month);
+
+        Select select_year = new Select(driver.findElement(select_years_field));
+        select_year.selectByValue(year);
+    }
 }
--- src/test/java/steps/CadastroSteps.java
+++ src/test/java/steps/CadastroSteps.java
@@ -3,13 +3,20 @@ package steps;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
+import pages.CadastroPage;
 import runner.RunCucumberTest;
 
 public class CadastroSteps extends RunCucumberTest {
 
+    CadastroPage cadastroPage = new CadastroPage(driver);
+
     @Quando("^eu preeencho o formulário de cadastro$")
     public void eu_preeencho_o_formulário_de_cadastro()  {
-
+        cadastroPage.selectTitle(1);
+        cadastroPage.preencheNome("Pedro");
+        cadastroPage.preencheSobrenome("Duarte");
+        cadastroPage.preenchePassword("123456");
+        cadastroPage.selecionaAniversario(20, 8, "1995");
     }
 
     @Quando("^clico em registrar$")
